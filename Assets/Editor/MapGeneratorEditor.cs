42b29882a9e5ae574414d71817a9c01e0c5ef7fc
antaires <11vdlo@gmail.com>
2020-04-19 09:37:19 +0200

    create map generator to produce 2D texture map using Perlin Noise

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (mapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+    public override void OnInspectorGUI(){
+        mapGenerator mapGenerator = (mapGenerator)target;
+
+        if (DrawDefaultInspector() ){
+            if (mapGenerator.autoUpdate){
+                mapGenerator.GenerateMap();
+
+            }
+        }
+         
+
+
+        if(GUILayout.Button("Generate Map")){
+            mapGenerator.GenerateMap();
+        }
+    }
+
+}
---
+++ Assets/scripts/mapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public bool autoUpdate;
+
+    public void GenerateMap(){
+        // fetch 2d noise map
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+
+}
